07d6ce4fc75d68a9e0a3ec041eebea32d8deff91
CRYPTO KNIGHT <isithaanusara01@gmail.com>
2025-03-26 10:23:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "𝐒𝐔𝐋𝐀-𝐌𝐃=7EgyHYBD#soS5PbCjsCzRJFmdg7f4tS9WxqIMjGVZ-F130Zd9myU", // ඔයාගේ session id එක දාන්න
-MONGODB: process.env.MONGODB || "mongodb://${{MONGO_INITDB_ROOT_USERNAME}}:${{MONGO_INITDB_ROOT_PASSWORD}}@${{RAILWAY_TCP_PROXY_DOMAIN}}:${{RAILWAY_TCP_PROXY_PORT}}", // ඔයාගේ mongodb url එක දාන්න
+MONGODB: process.env.MONGODB || "mongodb://mongo:WMHBPxpUZngirRYAyRdCYDsGTfQkdAAt@crossover.proxy.rlwy.net:56047", // ඔයාගේ mongodb url එක දාන්න
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/4g2tYcsx/1387.jpg",
 BOT_NAME: process.env.BOT_NAME || "𝐒𝐔𝐋𝐀-𝐌𝐃",
 LANG: process.env.BOT_LANG || 'EN' ,
